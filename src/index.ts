57a2e7ef70dce24322e5e29320ed995d56b28c7c
banzaiMimic <2274050+banzaiMimic@users.noreply.github.com>
2023-04-26 23:54:25 -0400

    add terminator for ctrl-c not ending processes correctly

--- src/index.ts
+++ src/index.ts
@@ -20,3 +20,18 @@ server.listen({ port: 8080 }, (err, address) => {
   }
   console.log(`Server listening at ${address}`)
 })
+
+// solves ctrl-c not ending process(es) correctly
+const terminator = (sig: any) => {
+  if (typeof sig === "string") {
+    process.exit(1)
+  }
+  console.log('%s: Node server stopped.', Date() )
+}
+
+// implement for every process signal related to exit/quit
+['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
+'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
+].forEach(function(element) {
+  process.on(element, function() { terminator(element) })
+})
